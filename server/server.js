15b04cb4e3b7666918179691007d775e62f3ce73
Treterten <mitch3moore@gmail.com>
2020-12-12 16:03:07 -0600

    Added loaderio functionality

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 const router = require('./router');
-
+require('dotenv').config({ path: '../config.env' });
 const PUBLIC_DIR = path.resolve(__dirname, '..', 'public');
 const app = express();
 
@@ -17,6 +17,10 @@ app.use('/api', router.api);
 
 app.get('/listing/*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '..', 'public/index.html'));
+});
+
+app.get('/loaderio-*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, `../${process.env.LOADERIO_FILENAME}`));
 });
 
 module.exports = app;
